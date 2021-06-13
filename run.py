2b861b0101caf7713946e513c63faf11c7403b49
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-06-13 13:11:57 +0100

    bugfix: actually import the new sub-module

--- run.py
+++ run.py
@@ -23,6 +23,7 @@ def run(flightLog = "/this/path/does/not/exist", extraCountries = [], notVisited
     # Import my modules ...
     try:
         import pyguymer3
+        import pyguymer3.geo
     except:
         raise Exception("\"pyguymer3\" is not installed; you need to have the Python module from https://github.com/Guymer/PyGuymer3 located somewhere in your $PYTHONPATH") from None
 
