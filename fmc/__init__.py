7f5c1f429826ffb438faf95330f6839637af5923
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-07-02 12:46:40 +0100

    replace os.path.join with f-strings

--- fmc/__init__.py
+++ fmc/__init__.py
@@ -6,9 +6,6 @@ the world with all of your flights overlaid and all of the countries that you
 have visited shaded in too.
 """
 
-# Import standard modules ...
-import sys
-
 # Import sub-functions ...
 from .calc_horizontal_gridlines import calc_horizontal_gridlines
 from .calc_vertical_gridlines import calc_vertical_gridlines
@@ -16,7 +13,3 @@ from .coordinates_of_IATA import coordinates_of_IATA
 from .country_of_IATA import country_of_IATA
 from .load_airport_list import load_airport_list
 from .run import run
-
-# Ensure that this module is only imported by Python 3.x ...
-if sys.version_info.major != 3:
-    raise Exception("the Python module \"fmc\" must only be used with Python 3.x") from None
