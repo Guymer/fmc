3ceec80485f3d9dc2325fb586117f5ec39e4f81a
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2023-02-03 09:01:59 +0000

    use proper idiom

--- example.py
+++ example.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
 
-# Import modules ...
-import fmc
+# Use the proper idiom in the main module ...
+# NOTE: See https://docs.python.org/3.10/library/multiprocessing.html#the-spawn-and-forkserver-start-methods
+if __name__ == "__main__":
+    # Import modules ...
+    import fmc
 
-# Run function ...
-fmc.run(
-    flightLog = "example.csv",
-    extraCountries = [
-        "Denmark",
-        "Germany",
-        "Ireland",
-        "Myanmar",
-        "Nepal",
-        "Netherlands",
-        "Russia",
-        "Switzerland",
-    ],
-    notVisited = [
-        "Hong Kong",
-        "Singapore",
-    ],
-    renames = {
-        "Czech Republic" : "Czechia",
-        "United States" : "United States of America",
-    }
-)
+    # Run function ...
+    fmc.run(
+        flightLog = "example.csv",
+        extraCountries = [
+            "Denmark",
+            "Germany",
+            "Ireland",
+            "Myanmar",
+            "Nepal",
+            "Netherlands",
+            "Russia",
+            "Switzerland",
+        ],
+        notVisited = [
+            "Hong Kong",
+            "Singapore",
+        ],
+        renames = {
+            "Czech Republic" : "Czechia",
+            "United States" : "United States of America",
+        }
+    )
