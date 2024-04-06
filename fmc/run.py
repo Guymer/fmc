f782431c06b091835f56a94f3e693e4ee1e115ca
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2024-04-06 14:45:20 +0100

    adapt to new optional keyword argument

--- fmc/run.py
+++ fmc/run.py
@@ -76,19 +76,19 @@ def run(flightLog, /, *, extraCountries = None, flightMap = None, notVisited = N
         fg,
         -97.0,
         +40.0,
-        2400.0e3,
-        nrows = 3,
-        ncols = 2,
+         dist = 2400.0e3,
         index = 3,
+        ncols = 2,
+        nrows = 3,
     )
     axR = pyguymer3.geo.add_topDown_axis(
         fg,
         +13.0,
         +54.0,
-        2000.0e3,
-        nrows = 3,
-        ncols = 2,
+         dist = 2000.0e3,
         index = 4,
+        ncols = 2,
+        nrows = 3,
     )
     axB = fg.add_subplot(
         3,
