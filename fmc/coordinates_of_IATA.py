940a3b2e1c471d10f8ef33bb97c07ae0ed7d5954
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2024-11-15 20:19:02 +0000

    add docstring; skip airports without IATA codes

--- fmc/coordinates_of_IATA.py
+++ fmc/coordinates_of_IATA.py
@@ -1,9 +1,44 @@
 #!/usr/bin/env python3
 
 # Define function ...
-def coordinates_of_IATA(airports, iata, /):
+def coordinates_of_IATA(
+    airports,
+    iata,
+    /,
+):
+    """Find the longitude and latitude of an airport
+
+    Parameters
+    ----------
+    airports : list
+        the list of all of the airports
+    iata : str
+        the IATA code of the desired airport
+
+    Returns
+    -------
+    lon : float
+        the longitude of the airport (in degrees)
+    lat : float
+        the latitude of the airport (in degrees)
+
+    Notes
+    -----
+    Copyright 2016 Thomas Guymer [1]_
+
+    References
+    ----------
+    .. [1] FMC, https://github.com/Guymer/fmc
+    """
+
+    # **************************************************************************
+
     # Loop over all airports ...
     for airport in airports:
+        # Skip this airport if it does not have an IATA code ...
+        if "IATA" not in airport:
+            continue
+
         # Check if this is the correct one and return it's coordinates ...
         if airport["IATA"] == iata:
             return airport["Longitude"], airport["Latitude"]
--- fmc/country_of_IATA.py
+++ fmc/country_of_IATA.py
@@ -1,9 +1,42 @@
 #!/usr/bin/env python3
 
 # Define function ...
-def country_of_IATA(airports, iata, /):
+def country_of_IATA(
+    airports,
+    iata,
+    /,
+):
+    """Find the country of an airport
+
+    Parameters
+    ----------
+    airports : list
+        the list of all of the airports
+    iata : str
+        the IATA code of the desired airport
+
+    Returns
+    -------
+    country : str
+        the country of the airport
+
+    Notes
+    -----
+    Copyright 2016 Thomas Guymer [1]_
+
+    References
+    ----------
+    .. [1] FMC, https://github.com/Guymer/fmc
+    """
+
+    # **************************************************************************
+    #
     # Loop over all airports ...
     for airport in airports:
+        # Skip this airport if it does not have an IATA code ...
+        if "IATA" not in airport:
+            continue
+
         # Check if this is the correct one and return it's country ...
         if airport["IATA"] == iata:
             return airport["Country"]
